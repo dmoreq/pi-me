13bd9cb0f17ed4a1378c0a3eb1f462ba17c7a7f3
Quy Doan <quy.doanm@gmail.com>
2026-05-15 17:13:26 +0700

    refactor(shared): remove obsolete notify-utils barrel

--- shared/index.ts
+++ shared/index.ts
@@ -10,7 +10,6 @@ export * from "./types";
 export * from "./audio.ts";
 export * from "./terminal.ts";
 export * from "./bg-notify-config.ts";
-export * from "./notify-utils";
 export * from "./lifecycle.ts";
 export * from "./telemetry-helpers.ts";
 export * from "./ext-state";
